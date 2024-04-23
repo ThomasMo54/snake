b72f18550138ec937d1881a74ef5ac7b5789e302
Thomas Moretti <thomas.moretti8@etu.univ-lorraine.fr>
2024-04-23 11:36:04 +0200

    feat: create GameObject class

---
+++ GameObject/GameObject.h
@@ -0,0 +1,30 @@
+#ifndef SNAKE_GAMEOBJECT_H
+#define SNAKE_GAMEOBJECT_H
+
+#include "../Math/Vector2i.h"
+
+/**
+ * The default any game object superclass.
+ */
+class GameObject {
+    Vector2i position;
+
+public:
+    /**
+     * Create a game object by giving its initial position.
+     * @param position
+     */
+    explicit GameObject(const Vector2i &position) {
+        this->position = Vector2i(position);
+    }
+
+    /**
+     * Get the current position of this game object.
+     * @return the position
+     */
+    const Vector2i& getPosition() const {
+        return position;
+    }
+};
+
+#endif //SNAKE_GAMEOBJECT_H
