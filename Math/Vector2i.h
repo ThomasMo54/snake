d5390822c7c070d8cb290c467078b52362b15100
Thomas Moretti <thomas.moretti8@etu.univ-lorraine.fr>
2024-04-23 11:24:13 +0200

    feat: comment Vector2i class

--- Math/Vector2i.h
+++ Math/Vector2i.h
@@ -6,20 +6,35 @@
 
 using namespace std;
 
+/**
+ * A minimalistic 2D vector class with integer components.
+ */
 class Vector2i {
 public:
     int x, y;
 
+    /**
+     * Construct a vector given its components.
+     * @param x the x component
+     * @param y the y component
+     */
     explicit Vector2i(int x, int y) {
         this->x = x;
         this->y = y;
     }
 
+    /**
+     * Construct a new vector by copying the given one.
+     * @param v the vector to copy
+     */
     Vector2i(const Vector2i &v) {
         x = v.x;
         y = v.y;
     }
 
+    /**
+     * Construct a default vector with its components set to 0.
+     */
     explicit Vector2i() {
         x = 0;
         y = 0;
